e620e2218f4dc86699d3f1db4b8690920ee56935
Léona Chevrel <chevrel.leona@gmail.com>
2018-07-09 15:44:12 +0200

    04_simon_says réussis sauf titleize

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -1,1 +1,23 @@
-#write your code here
+def echo(str)
+  return str
+end
+
+def shout(str)
+  return str.upcase
+end
+
+def repeat(str, times = 2)
+  return ((str + " ") * times).chop
+end
+
+def start_of_word(str, letters)
+  return str[0, letters]
+end
+
+def first_word(str)
+  return str.split.first
+end
+
+def titleize(str)
+  return str.split(/ |\_/).map(&:capitalize).join(" ")
+end
